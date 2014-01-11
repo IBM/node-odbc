3fce14703f1b0b9edcf038ade88fbecb57c45899
Lee Houghton <lee@theconnectgroup.net>
2014-01-11 20:06:52 +0000

    Fix test-param-select-with-numbers-only on MSSQL
    Precision again.

--- test/test-param-select-with-numbers-only.js
+++ test/test-param-select-with-numbers-only.js
@@ -11,14 +11,23 @@ db.open(common.connectionString, function (err) {
     , [5, 3, 1, 1.23456789012345, 12345.000]
     , function (err, data, more) {
         db.close(function () {
+          console.log(data);
           assert.equal(err, null);
-          assert.deepEqual(data, [{
-            INTCOL1: 5,
-            INTCOL2: 3,
-            INTCOL3: 1,
-            FLOATCOL4 : 1.23456789012345,
-            FLOATYINT : 12345
-          }]);
+          assert(Array.isArray(data), 'data is not an array')
+          assert(typeof data[0] === 'object', 'data[0] is not an object');
+          assert.strictEqual(data[0].INTCOL1, 5, 'INTCOL1 is wrong');
+          assert.strictEqual(data[0].INTCOL2, 3, 'INTCOL2 is wrong');
+          assert.strictEqual(data[0].INTCOL3, 1, 'INTCOL3 is wrong');
+          assert.equal(Math.round(data[0].FLOATCOL4 * 1000000) / 1000000, 1.234568); // In case the database's precision is not stellar...
+          assert.strictEqual(data[0].FLOATYINT, 12345, 'FLOATYINT is wrong');
+
+          //assert.deepEqual(data, [{
+          //  INTCOL1: 5,
+          //  INTCOL2: 3,
+          //  INTCOL3: 1,
+          //  FLOATCOL4 : 1.23456789012345,
+          //  FLOATYINT : 12345
+          //}]);
         });
     });
 });
