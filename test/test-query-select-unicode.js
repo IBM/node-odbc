c0cbf24d5dc1ccf62314e949532acc530cbe45b0
Lee Houghton <lee@theconnectgroup.net>
2014-01-11 20:25:00 +0000

    Fix test-query-select-unicode on MSSQL
    MSSQL requires N'☯ąčęėįšųūž☎áäàéêèóöòüßÄÖÜ€ шчябы Ⅲ ❤' but sqlite
    doesn't support national character literals. Relying on default
    collation being unicode-capable is also non-standard, however...

--- test/test-query-select-unicode.js
+++ test/test-query-select-unicode.js
@@ -6,7 +6,7 @@ var common = require("./common")
 
 db.openSync(common.connectionString);
 
-db.query("select '☯ąčęėįšųūž☎áäàéêèóöòüßÄÖÜ€ шчябы Ⅲ ❤' as UNICODETEXT", function (err, data) {
+db.query("select ? as UNICODETEXT", ['☯ąčęėįšųūž☎áäàéêèóöòüßÄÖÜ€ шчябы Ⅲ ❤'], function (err, data) {
   db.closeSync();
   console.log(data);
   assert.equal(err, null);
