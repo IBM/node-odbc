53d4a2353445325e610444dc7c5a39bb43eb3679
Dan VerWeire <dverweire@gmail.com>
2015-05-13 15:06:24 -0400

    GetColumnValue: modify SQL_INTEGER handling to use a int32_t and return a NanNew<Integer>

--- src/odbc.cpp
+++ src/odbc.cpp
@@ -365,7 +365,7 @@ Handle<Value> ODBC::GetColumnValue( SQLHSTMT hStmt, Column column,
     case SQL_INTEGER : 
     case SQL_SMALLINT :
     case SQL_TINYINT : {
-        long value = 0;
+        int32_t value = 0;
         
         ret = SQLGetData(
           hStmt, 
@@ -375,14 +375,14 @@ Handle<Value> ODBC::GetColumnValue( SQLHSTMT hStmt, Column column,
           sizeof(value), 
           &len);
         
-        DEBUG_PRINTF("ODBC::GetColumnValue - Integer: index=%i name=%s type=%i len=%i ret=%i\n", 
-                    column.index, column.name, column.type, len, ret);
+        DEBUG_PRINTF("ODBC::GetColumnValue - Integer: index=%i name=%s type=%i len=%i ret=%i val=%li\n", 
+                    column.index, column.name, column.type, len, ret, value);
         
         if (len == SQL_NULL_DATA) {
           return NanEscapeScope(NanNull());
         }
         else {
-          return NanEscapeScope(NanNew<Number>(value));
+          return NanEscapeScope(NanNew<Integer>(value));
         }
       }
       break;
