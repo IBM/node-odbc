f98cf367c1d9fa9ca2516ab0d2dead2b80f7848a
Dan VerWeire <dverweire@gmail.com>
2015-05-13 13:02:25 -0400

    Update test to include min and max of integer range

--- test/test-issue-85.js
+++ test/test-issue-85.js
@@ -7,8 +7,8 @@ var common = require("./common")
   ;
  
 var sql = (common.dialect == 'sqlite' || common.dialect =='mysql')
-  ? 'select cast(-1 as signed) as test;'
-  : 'select cast(-1 as int) as test;'
+  ? 'select cast(-1 as signed) as test, cast(-2147483648 as signed) as test2, cast(2147483647 as signed) as test3;'
+  : 'select cast(-1 as int) as test, cast(-2147483648 as int) as test2, cast(2147483647 as int) as test3;'
   ;
 
 db.open(common.connectionString, function(err) {
@@ -16,8 +16,12 @@ db.open(common.connectionString, function(err) {
 
   if (!err) {
     db.query(sql, function (err, results, more) {
+      console.log(results);
+
       assert.equal(err, null);
       assert.equal(results[0].test, -1);
+      assert.equal(results[0].test2, -2147483648);
+      assert.equal(results[0].test3, 2147483647);
 
       db.close(function(err) { console.log(err || "Closed") })
     })
