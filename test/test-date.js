b2fe3c179c54263dc379c73485267e6d81f4b790
Ben Noordhuis <info@bnoordhuis.nl>
2017-03-06 20:09:23 +0100

    Fix test-date failure.
    Retrieved dates are in local time but we insert a UTC date so adjust it
    before comparing.

--- test/test-date.js
+++ test/test-date.js
@@ -11,6 +11,7 @@ db.open(common.connectionString, function(err) {
   assert.equal(db.connected, true);
   
   var dt = new Date();
+  dt.setMilliseconds(0);  // MySQL truncates them.
   var ds = dt.toISOString().replace('Z','');
   var sql = "SELECT cast('" + ds + "' as datetime) as DT1";
   // XXX(bnoordhuis) sqlite3 has no distinct DATETIME or TIMESTAMP type.
@@ -34,7 +35,10 @@ db.open(common.connectionString, function(err) {
         assert.equal(data[0].DT1, ds.replace('T', ' ').replace(/\.\d+$/, ''));
       } else {
         assert.equal(data[0].DT1.constructor.name, "Date", "DT1 is not an instance of a Date object");
-        assert.equal(data[0].DT1.getTime(), dt.getTime());
+        // XXX(bnoordhuis) DT1 is in local time but we inserted
+        // a UTC date so we need to adjust it before comparing.
+        dt = new Date(dt.getTime() + 6e4 * dt.getTimezoneOffset());
+        assert.equal(data[0].DT1.toISOString(), dt.toISOString());
       }
     });
   });
