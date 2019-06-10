b82bbf3aa4ca7ff81f388e50159402a49e318944
Mark Irish <mirish@ibm.com>
2019-06-10 10:24:27 -0500

    Ooops, removing my test javascript file

--- app.js
+++
@@ -1,9 +0,0 @@
-const odbc = require('.');
-
-async function doit() {
-  const connection = await odbc.connect('DSN=OSS73DEV');
-  const result = await connection.query('SELECT * FROM MIRISH.TEST');
-  console.log(result);
-}
-
-doit();
