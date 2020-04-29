aecac2dfd9af695a0c2cfb32caf9d0854d64a1a7
Mark Irish <mirish@ibm.com>
2020-04-29 10:33:58 -0500

    Include custom TypeScript classes (Result, NodeOdbcError, OdbcError) to make it easier to interact with values returned from functions
    Signed-off-by: Mark Irish <mirish@ibm.com>

--- lib/odbc.d.ts
+++ lib/odbc.d.ts
@@ -1,71 +1,95 @@
 declare namespace odbc {
+
+  class ColumnDefinition {
+    name: string;
+    datayType: number;
+  }
+
+  class Result extends Array<object> {
+    count: number;
+    columns: Array<ColumnDefinition>;
+    statement: string;
+    parameters: Array<number|string>;
+    return: number;
+  }
+
+  class OdbcError {
+    message: string;
+    code:    number;
+    state:   string;
+  }
+
+  class NodeOdbcError extends Error {
+    odbcErrors: Array<OdbcError>;
+  }
+
   class Statement {
 
-    //////////////////////////////////////////////////////////////////////////////
-    //   Callbacks   /////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    //   Callbacks   ///////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
 
-    prepare(sql:string, callback: (error: Error) => undefined): undefined;
+    prepare(sql:string, callback: (error: NodeOdbcError) => undefined): undefined;
 
-    bind(parameters: Array<number|string>, callback: (error: Error) => undefined): undefined;
+    bind(parameters: Array<number|string>, callback: (error: NodeOdbcError) => undefined): undefined;
 
-    execute(callback: (error: Error, result: Array<any>) => undefined): undefined;
+    execute(callback: (error: NodeOdbcError, result: Result) => undefined): undefined;
 
-    close(callback: (error: Error) => undefined): undefined;
+    close(callback: (error: NodeOdbcError) => undefined): undefined;
 
-    //////////////////////////////////////////////////////////////////////////////
-    //   Promises   //////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    //   Promises   ////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
 
     prepare(sql:string): Promise<void>;
   
     bind(parameters: Array<number|string>): Promise<void>;
   
-    execute(): Promise<Array<any>>;
+    execute(): Promise<Result>;
   
     close(): Promise<void>;
   }
 
   class Connection {
 
-    //////////////////////////////////////////////////////////////////////////////
-    //   Callbacks   /////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////
-    query(sql: string, callback: (error: Error, result: Array<any>) => undefined): undefined;
-    query(sql: string, parameters: Array<number|string>, callback: (error: Error, result: Array<any>) => undefined): undefined;
+    ////////////////////////////////////////////////////////////////////////////
+    //   Callbacks   ///////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    query(sql: string, callback: (error: NodeOdbcError, result:Result) => undefined): undefined;
+    query(sql: string, parameters: Array<number|string>, callback: (error: NodeOdbcError, result: Result) => undefined): undefined;
 
-    callProcedure(catalog: string, schema: string, name: string, callback: (error: Error, result: Array<any>) => undefined): undefined;
-    callProcedure(catalog: string, schema: string, name: string, parameters: Array<number|string>, callback: (error: Error, result: Array<any>) => undefined): undefined;
+    callProcedure(catalog: string, schema: string, name: string, callback: (error: NodeOdbcError, result: Result) => undefined): undefined;
+    callProcedure(catalog: string, schema: string, name: string, parameters: Array<number|string>, callback: (error: NodeOdbcError, result: Result) => undefined): undefined;
 
-    createStatement(callback: (error: Error, statement: Statement) => undefined): undefined;
+    createStatement(callback: (error: NodeOdbcError, statement: Statement) => undefined): undefined;
 
-    tables(catalog: string, schema: string, table: string, callback: (error: Error, result: Array<any>) => undefined): undefined;
+    tables(catalog: string, schema: string, table: string, callback: (error: NodeOdbcError, result: Result) => undefined): undefined;
 
-    columns(catalog: string, schema: string, table: string, callback: (error: Error, result: Array<any>) => undefined): undefined;
+    columns(catalog: string, schema: string, table: string, callback: (error: NodeOdbcError, result: Result) => undefined): undefined;
 
-    setIsolationLevel(level: number, callback: (error: Error) => undefined): undefined;
+    setIsolationLevel(level: number, callback: (error: NodeOdbcError) => undefined): undefined;
 
-    beginTransaction(callback: (error: Error) => undefined): undefined;
+    beginTransaction(callback: (error: NodeOdbcError) => undefined): undefined;
 
-    commit(callback: (error: Error) => undefined): undefined;
+    commit(callback: (error: NodeOdbcError) => undefined): undefined;
 
-    rollback(callback: (error: Error) => undefined): undefined;
+    rollback(callback: (error: NodeOdbcError) => undefined): undefined;
 
-    close(callback: (error: Error) => undefined): undefined;
+    close(callback: (error: NodeOdbcError) => undefined): undefined;
 
-    //////////////////////////////////////////////////////////////////////////////
-    //   Promises   //////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    //   Promises   ////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
 
-    query(sql: string, parameters?: Array<number|string>): Promise<Array<any>>;
+    query(sql: string, parameters?: Array<number|string>): Promise<Result>;
 
-    callProcedure(catalog: string, schema: string, name: string, parameters?: Array<number|string>): Promise<Array<any>>;
+    callProcedure(catalog: string, schema: string, name: string, parameters?: Array<number|string>): Promise<Result>;
 
     createStatement(): Promise<Statement>;
 
-    tables(catalog: string, schema: string, table: string): Promise<Array<any>>;
+    tables(catalog: string, schema: string, table: string): Promise<Result>;
 
-    columns(catalog: string, schema: string, table: string, column: string): Promise<Array<any>>;
+    columns(catalog: string, schema: string, table: string, column: string): Promise<Result>;
 
     setIsolationLevel(level: number): Promise<void>;
 
@@ -80,36 +104,36 @@ declare namespace odbc {
 
   class Pool {
 
-    //////////////////////////////////////////////////////////////////////////////
-    //   Callbacks   /////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////
-    connect(callback: (error: Error, connection: Connection) => undefined): undefined;
+    ////////////////////////////////////////////////////////////////////////////
+    //   Callbacks   ///////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    connect(callback: (error: NodeOdbcError, connection: Connection) => undefined): undefined;
 
-    query(sql: string, callback: (error: Error, result: Array<any>) => undefined): undefined;
-    query(sql: string, parameters: Array<number|string>, callback: (error: Error, result: Array<any>) => undefined): undefined;
+    query(sql: string, callback: (error: NodeOdbcError, result: Result) => undefined): undefined;
+    query(sql: string, parameters: Array<number|string>, callback: (error: NodeOdbcError, result: Array<any>) => undefined): undefined;
 
-    close(callback: (error: Error) => undefined): undefined;
+    close(callback: (error: NodeOdbcError) => undefined): undefined;
 
 
-    //////////////////////////////////////////////////////////////////////////////
-    //   Promises   //////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    //   Promises   ////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
     connect(): Promise<Connection>;
 
-    query(sql: string, parameters?: Array<number|string>): Promise<Array<any>>;
+    query(sql: string, parameters?: Array<number|string>): Promise<Result>;
 
     close(): Promise<void>;
   }
 
-  function connect(connectionString: string, callback: (error: Error, connection: Connection) => undefined): undefined;
-  function connect(connectionObject: object, callback: (error: Error, connection: Connection) => undefined): undefined;
+  function connect(connectionString: string, callback: (error: NodeOdbcError, connection: Connection) => undefined): undefined;
+  function connect(connectionObject: object, callback: (error: NodeOdbcError, connection: Connection) => undefined): undefined;
 
   function connect(connectionString: string): Promise<Connection>;
   function connect(connectionObject: object): Promise<Connection>;
 
 
-  function pool(connectionString: string, callback: (error: Error, pool: Pool) => undefined): undefined;
-  function pool(connectionObject: object, callback: (error: Error, pool: Pool) => undefined): undefined;
+  function pool(connectionString: string, callback: (error: NodeOdbcError, pool: Pool) => undefined): undefined;
+  function pool(connectionObject: object, callback: (error: NodeOdbcError, pool: Pool) => undefined): undefined;
 
   function pool(connectionString: string): Promise<Pool>;
   function pool(connectionObject: object): Promise<Pool>;
