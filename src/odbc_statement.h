b2365e2ce603591b79b64d47baaba4b1f53992c9
Mark Irish <mirish@ibm.com>
2021-01-28 11:11:09 -0600

    Remove unneeded comma, add beginning year to copyright
    Signed-off-by: Mark Irish <mirish@ibm.com>

--- src/odbc_statement.h
+++ src/odbc_statement.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2021, IBM
+  Copyright (c) 2019, 2021 IBM
   Copyright (c) 2013, Dan VerWeire<dverweire@gmail.com>
 
   Permission to use, copy, modify, and/or distribute this software for any
